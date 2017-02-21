20a71b31868a59d1f1b0d49cb2baa6fb4790ec30
sippun <sippun@users.noreply.github.com>
2017-02-21 10:41:58 -0800

    Add GridView to MainActivity

---
+++ app/src/main/java/com/example/android/popularmovies/MovieFragment.java
@@ -0,0 +1,32 @@
+package com.example.android.popularmovies;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Joel on 2/21/2017.
+ */
+
+public class MovieFragment extends Fragment {
+
+    public MovieFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
