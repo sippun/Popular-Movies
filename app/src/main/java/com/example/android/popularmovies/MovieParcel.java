674c04c76c186d0a752144e12edf5dabfc7915f6
sippun <sippun@users.noreply.github.com>
2017-03-02 11:16:02 -0800

    Create MovieParcel

---
+++ app/src/main/java/com/example/android/popularmovies/MovieParcel.java
@@ -0,0 +1,51 @@
+package com.example.android.popularmovies;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by Joel on 3/2/2017.
+ */
+
+public class MovieParcel implements Parcelable {
+    String movieID;
+    String posterPath;
+
+    public MovieParcel(String id, String path) {
+        this.movieID = id;
+        this.posterPath = path;
+    }
+
+    private MovieParcel(Parcel in) {
+        movieID = in.readString();
+        posterPath = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public String toString() {
+        return movieID + "--" + posterPath;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(movieID);
+        dest.writeString(posterPath);
+    }
+
+    public final Parcelable.Creator<MovieParcel> CREATOR = new Parcelable.Creator<MovieParcel>() {
+
+        @Override
+        public MovieParcel createFromParcel(Parcel source) {
+            return new MovieParcel(source);
+        }
+
+        @Override
+        public MovieParcel[] newArray(int i) {
+            return new MovieParcel[i];
+        }
+    }
+}
